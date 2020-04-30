d2a5ae2b27948198413b31e9b35c42c2741a7ad2
pedrozc90 <pedrozc90@gmail.com>
2020-04-30 09:19:32 -0300

    project controller ajustements

--- src/controllers/projects/ProjectCtrl.ts
+++ src/controllers/projects/ProjectCtrl.ts
@@ -1,16 +1,17 @@
 import { Controller, Get, QueryParams, PathParams, Delete, Post, BodyParams, Locals } from "@tsed/common";
 
-import { ProjectRepository, ExtensionLineRepository } from "../../repositories";
-import { Page, Project, ExtensionLine } from "../../entities";
+import { ProjectRepository, ExtensionLineRepository, KnowledgeAreaRepository, ThemeAreaRepository } from "../../repositories";
+import { Page, Project, ExtensionLine, KnowledgeArea } from "../../entities";
 import { IOptions, IContext, Scope } from "../../types";
 import { CustomAuth } from "../../services";
-import { Like } from "typeorm";
 
 @Controller("/projects")
 export class ProjectCtrl {
 
     constructor(private projectRepository: ProjectRepository,
-        private extensionLineRepository: ExtensionLineRepository) {}
+        private extensionLineRepository: ExtensionLineRepository,
+        private knowledgeAreaRepository: KnowledgeAreaRepository,
+        private themeAreaRepository: ThemeAreaRepository) {}
 
     @Get("/")
     @CustomAuth({ scope: [] })
@@ -22,10 +23,10 @@ export class ProjectCtrl {
 
         let query = await this.projectRepository.createQueryBuilder("p");
 
-        query = query.innerJoinAndSelect("p.projectHumanResources", "phr")
-            .innerJoinAndSelect("phr.user", "usr")
-            .leftJoinAndSelect("usr.collaborator", "clb")
-            .leftJoinAndSelect("usr.student", "std");
+        query = query.leftJoinAndSelect("p.projectHumanResources", "phr")
+            .innerJoin("phr.user", "usr")
+            .leftJoin("usr.collaborator", "clb")
+            .leftJoin("usr.student", "std");
 
         if (context.scope !== Scope.ADMINISTRATOR) {
             query = query.where("usr.id = :userId", { userId: context.user?.id });
@@ -45,30 +46,36 @@ export class ProjectCtrl {
         return this.projectRepository.save(project);
     }
 
-    @Get("/list")
-    @CustomAuth({ scope: [] })
-    public async list(@Locals("context") context: IContext, @QueryParams("q") q: string): Promise<Project[]> {
-        let query = await this.projectRepository.createQueryBuilder("p");
-        
-        query = query.innerJoinAndSelect("p.projectHumanResources", "phr")
-            .innerJoinAndSelect("phr.user", "usr")
-            .leftJoinAndSelect("usr.collaborator", "clb")
-            .leftJoinAndSelect("usr.student", "std");
-
-        if (context?.scope !== Scope.ADMINISTRATOR) {
-            query = query.where("usr.id = :userId", { userId: context.user?.id });
-        }
-
-        if (q) {
-            query = query.where(`p.title like %${q}%`);
-        }
-
-        return query.getMany();
-    }
-
     @Get("/:id")
-    public async get(@PathParams("id") id: number): Promise<Project | undefined> {
-        return this.projectRepository.findById(id);
+    @CustomAuth({ scope: [] })
+    public async get(@Locals("context") context: IContext, @PathParams("id") id: number): Promise<Project | undefined> {
+        let query = await this.projectRepository.createQueryBuilder("p")
+            .leftJoinAndSelect("p.activities", "activities")
+            .leftJoinAndSelect("p.attachments", "attachments")
+            .leftJoinAndSelect("p.demands", "demands")
+            .leftJoinAndSelect("p.disclosureMedias", "disclosureMedias")
+            .leftJoinAndSelect("p.evaluations", "evaluations")
+            .leftJoinAndSelect("p.eventPresentations", "eventPresentations")
+            .leftJoinAndSelect("p.extensionLines", "extensionLines")
+            .leftJoinAndSelect("p.futureDevelopmentPlans", "futureDevelopmentPlans")
+            .leftJoinAndSelect("p.knowledgeAreas", "knowledgeAreas")
+            .leftJoinAndSelect("p.partners", "partners")
+            .leftJoinAndSelect("p.projectHumanResources", "projectHumanResources")
+            .leftJoinAndSelect("p.projectPublics", "projectPublics")
+            .leftJoinAndSelect("p.projectTargets", "projectTargets")
+            .leftJoinAndSelect("p.projectThemeAreas", "projectThemeAreas")
+            .leftJoinAndSelect("p.publications", "publications")
+            
+            .leftJoinAndSelect("projectHumanResources.user", "user")
+            .leftJoinAndSelect("user.collaborator", "collaborator")
+            .leftJoinAndSelect("user.student", "student")
+            .where("p.id = :id", { id });
+
+        if (context.scope !== Scope.ADMINISTRATOR) {
+            query = query.where("user.id = :userId", { userId: context.user?.id });
+        }
+
+        return query.getOne();
     }
 
     @Delete("/:id")
@@ -86,5 +93,45 @@ export class ProjectCtrl {
             where: { project: { id } }
         });
     }
+
+    @Get("/:id/knowledge-areas")
+    public async getKnowledgeAreas(@PathParams("id") id: number): Promise<any> {
+        return this.knowledgeAreaRepository.find({
+            join: {
+                alias: "ka",
+                innerJoin: { project: "ka.projects" }
+            },
+            where: { project: { id } }
+        });
+    }
+
+    @Get("/:id/theme-areas")
+    public async getThemeAreas(@PathParams("id") id: number): Promise<any> {
+        return this.themeAreaRepository.find({
+            join: {
+                alias: "ta",
+                innerJoin: {
+                    projectThemeArea: "ta.projectThemeAreas",
+                    project: "projectThemeArea.project"
+                }
+            },
+            where: { project: { id } },
+            relations: [ "projectThemeAreas" ]
+        });
+    }
+
+    // @Get("/:id/test")
+    // public async getTest(@PathParams("id") id: number): Promise<any> {
+    //     return this.projectRepository.find({
+    //         join: {
+    //             alias: "p",
+    //             leftJoinAndSelect: {
+    //                 projectThemeArea: "p.projectThemeAreas",
+    //                 themeArea: "projectThemeArea.themeArea"
+    //             }
+    //         },
+    //         where: { id }
+    //     });
+    // }
 
 }
