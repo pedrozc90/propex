e0f5d302d379453e44120c9a5931a0c58e917cbb
pedrozc90 <pedrozc90@gmail.com>
2020-05-23 23:46:36 -0300

    global middlewares

--- src/middlewares/GlobalSendResponseMiddleware.ts
+++ src/middlewares/GlobalSendResponseMiddleware.ts
@@ -1,31 +1,31 @@
-// import { ConverterService, IMiddleware, OverrideMiddleware, Req, Res, SendResponseMiddleware } from "@tsed/common";
-// import { isBoolean, isNumber, isStream, isString } from "@tsed/core";
+import { ConverterService, IMiddleware, OverrideMiddleware, Req, Res, SendResponseMiddleware } from "@tsed/common";
+import { isBoolean, isNumber, isStream, isString } from "@tsed/core";
 
-// import { ResultContent } from "../models/generics/ResultContent";
+import { ResultContent } from "../entities";
 
-// @OverrideMiddleware(SendResponseMiddleware)
-// export class GlobalSendResponseMiddleware implements IMiddleware {
+@OverrideMiddleware(SendResponseMiddleware)
+export class GlobalSendResponseMiddleware implements IMiddleware {
 
-//     constructor(protected converterService: ConverterService) {}
+    constructor(protected converterService: ConverterService) {}
 
-//     public use(@Req() request: Req, @Res() response: Res) {
-//         const { data, endpoint } = request.ctx;
-//         if (data === undefined) {
-//             return response.send();
-//         }
+    public use(@Req() request: Req, @Res() response: Res) {
+        const { data, endpoint } = request.ctx;
+        if (data === undefined) {
+            return response.send();
+        }
 
-//         if (isStream(data)) {
-//             data.pipe(response);
-//             return response;
-//         }
+        if (isStream(data)) {
+            data.pipe(response);
+            return response;
+        }
 
-//         if (isBoolean(data) || isNumber(data) || isString(data) || data === null) {
-//             return response.send(ResultContent.of(data));
-//         }
+        if (isBoolean(data) || isNumber(data) || isString(data) || data === null) {
+            return response.send(ResultContent.of(data));
+        }
 
-//         const content = (data instanceof ResultContent) ? data : ResultContent.of<any>(data);
+        const content = (data instanceof ResultContent) ? data : ResultContent.of<any>(data);
 
-//         return response.json(this.converterService.serialize(content, { type: endpoint.type }));
-//     }
+        return response.json(this.converterService.serialize(content, { type: endpoint.type }));
+    }
 
-// }
+}
